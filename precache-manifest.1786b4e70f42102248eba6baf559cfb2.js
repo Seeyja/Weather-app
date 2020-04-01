76d61a0421d9e26cf8083d8b3b92652f0e5066a2
Sebastian Tomaszkiewicz <sebastianmtomaszkiewicz@gmail>
2020-04-01 15:16:26 +0200

    Updates

---
+++ precache-manifest.1786b4e70f42102248eba6baf559cfb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9408810b8ddf6d36bfda8c78f82034",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "66b32a7d16ee45e1ead9",
+    "url": "/Weather-app/static/css/main.6835100f.chunk.css"
+  },
+  {
+    "revision": "8983b17c9b413de2bd7a",
+    "url": "/Weather-app/static/js/2.3e0e8b80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-app/static/js/2.3e0e8b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b32a7d16ee45e1ead9",
+    "url": "/Weather-app/static/js/main.e4777435.chunk.js"
+  },
+  {
+    "revision": "3e01455b39c3f7d2a4d2",
+    "url": "/Weather-app/static/js/runtime-main.4846eb58.js"
+  }
+]);
